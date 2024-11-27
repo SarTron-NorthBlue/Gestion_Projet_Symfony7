12c271ca691a275ac62f0eff04e5ff79e1b56edd
Sar_Tron <north.blueserveur@gmail.com>
2024-11-27 11:36:43 +0400

    ajout du système de permission part page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,11 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+        // Vérifie si l'utilisateur a le rôle ROLE_USER ou ROLE_ADMIN
+        if (!$this->isGranted('ROLE_USER') && !$this->isGranted('ROLE_ADMIN')) {
+            throw $this->createAccessDeniedException('Vous n\'avez pas accès à cette page.');
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
