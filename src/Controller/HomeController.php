5589e6a5b220a845ebff2c63598ab753b4286775
Sar_Tron <north.blueserveur@gmail.com>
2024-11-28 11:30:11 +0400

    ajout de planning des tache pour afficher les tâche en fonctione des utilisateur

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,22 +2,36 @@
 
 namespace App\Controller;
 
+use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(TaskRepository $taskRepository): Response
     {
-        // Vérifie si l'utilisateur a le rôle ROLE_USER ou ROLE_ADMIN
-        if (!$this->isGranted('ROLE_USER') && !$this->isGranted('ROLE_ADMIN')) {
-            throw $this->createAccessDeniedException('Vous n\'avez pas accès à cette page.');
+        // Récupérer l'utilisateur connecté
+        $user = $this->getUser();
+
+        // Si l'utilisateur n'est pas connecté, on refuse l'accès
+        if (!$user) {
+            throw $this->createAccessDeniedException('Vous devez être connecté pour accéder à cette page.');
         }
 
+        // Récupérer les tâches assignées à cet utilisateur
+        $tasks = $taskRepository->findBy(['user' => $user]);
+
+        // Vérification de l'existence de $tasks
+        if (!isset($tasks)) {
+            $tasks = []; // Définit une valeur par défaut si aucune tâche n'est trouvée
+        }
+
+        // Rendre le template et passer les tâches
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'tasks' => $tasks, // Passe les tâches au template
         ]);
     }
 }
